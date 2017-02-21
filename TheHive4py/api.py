1add2be75acc03ba6984af216ee4290417c5904e
Jerome Leonard <jerome@upbeat.fr>
2017-02-21 15:08:02 +0100

    add proxies configuration

--- TheHive4py/api.py
+++ TheHive4py/api.py
@@ -22,11 +22,12 @@ class TheHiveApi():
         :param password: password
     """
 
-    def __init__(self, url, username, password):
+    def __init__(self, url, username, password, proxies):
 
         self.url = url
         self.username = username
         self.password = password
+        self.proxies = proxies
         self.session = requests.Session()
         self.auth = requests.auth.HTTPBasicAuth(username=self.username,
                                                 password=self.password)
@@ -34,22 +35,22 @@ class TheHiveApi():
     def create_case(self, case):
         req = self.url + "/api/case"
         data = case.jsonify()
-        return self.session.post(req, headers={'Content-Type': 'application/json'}, data=data, auth=self.auth)
+        return self.session.post(req, headers={'Content-Type': 'application/json'}, data=data, proxies=self.proxies, auth=self.auth)
 
     def create_case_task(self, id, caseTask):
         req = self.url + "/api/case/{}/task".format(id)
         data = caseTask.jsonify()
-        return self.session.post(req, headers={'Content-Type': 'application/json'}, data=data, auth=self.auth)
+        return self.session.post(req, headers={'Content-Type': 'application/json'}, data=data, proxies=self.proxies, auth=self.auth)
 
     def create_task_log(self, taskId, caseTaskLog):
         req = self.url + "/api/case/task/{}/log".format(taskId)
         data = caseTaskLog.jsonify()
-        return self.session.post(req, headers={'Content-Type': 'application/json'}, data=data, auth=self.auth)
+        return self.session.post(req, headers={'Content-Type': 'application/json'}, data=data, proxies=self.proxies, auth=self.auth)
 
     def get_case(self, id):
         req = self.url + "/api/case/{}".format(id)
 
-        return self.session.get(req, auth=self.auth)
+        return self.session.get(req, proxies=self.proxies, auth=self.auth)
 
     def get_case_observables(self, id):
         req = self.url + "/api/case/artifact/_search"
@@ -68,7 +69,7 @@ class TheHiveApi():
             }
         }
 
-        return self.session.post(req, json=data, auth=self.auth)
+        return self.session.post(req, json=data, proxies=self.proxies, auth=self.auth)
 
 
 # - createCase()
