0467435deb7250cfcb0d550d1c2d0011a367b165
Nabil Adouani <nabil.adouani@gmail.com>
2017-09-17 23:45:32 +0200

    Bump the hotfix version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 setup(
     name='thehive4py',
-    version='1.3.0',
+    version='1.3.1',
     description='Python API client for TheHive.',
     long_description=read_md('README.md'),
     author='TheHive-Project',
