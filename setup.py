55ab67f4debfbd11a2eb0dfc6379dd38453df2ce
Nabil Adouani <nabil.adouani@gmail.com>
2018-11-12 10:43:03 +0100

    Bump version to 1.5.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 setup(
     name='thehive4py',
-    version='1.5.1',
+    version='1.5.2',
     description='Python API client for TheHive.',
     long_description=read_md('README.md'),
     author='TheHive-Project',
