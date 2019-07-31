9ddc8b6a30d82a543fc8cb1d8d96bfe46cc7d26b
Zachary Priddy <github@zpriddy.com>
2019-07-31 13:15:32 -0700

    Update api.py
    Add customFields to alert update

--- thehive4py/api.py
+++ thehive4py/api.py
@@ -415,7 +415,7 @@ class TheHiveApi:
         req = self.url + "/api/alert/{}".format(alert_id)
 
         # update only the alert attributes that are not read-only
-        update_keys = ['tlp', 'severity', 'tags', 'caseTemplate', 'title', 'description']
+        update_keys = ['tlp', 'severity', 'tags', 'caseTemplate', 'title', 'description', 'customFields']
 
         data = {k: v for k, v in alert.__dict__.items() if
                 (len(fields) > 0 and k in fields) or (len(fields) == 0 and k in update_keys)}
