63ea569b1ff9ce3d1e8ddd2beffef57b04756608
Kamforka <kamforka@hotmail.com>
2022-02-22 20:39:46 +0100

    Implement task endpoints

--- thehive4py/endpoints/task.py
+++ thehive4py/endpoints/task.py
@@ -5,15 +5,97 @@ from thehive4py.query import QueryExpr
 from thehive4py.query.filters import FilterExpr
 from thehive4py.query.page import Paginate
 from thehive4py.query.sort import SortExpr
-from thehive4py.types.task import OutputTask
+from thehive4py.types.task import (
+    InputBulkUpdateTask,
+    InputTask,
+    InputUpdateTask,
+    OutputTask,
+)
+from thehive4py.types.task_log import InputTaskLog, OutputTaskLog
 
 
 class TaskEndpoint(EndpointBase):
+    def create(self, case_id: str, task: InputTask) -> OutputTask:
+        return self._session.make_request(
+            "POST", path=f"/api/v1/case/{case_id}/task", json=task
+        )
+
     def get(self, task_id: str) -> OutputTask:
         return self._session.make_request("GET", path=f"/api/v1/task/{task_id}")
 
     def delete(self, task_id: str) -> None:
-        self._session.make_request("DELETE", path=f"/api/v1/task/{task_id}")
+        return self._session.make_request("DELETE", path=f"/api/v1/task/{task_id}")
+
+    def update(self, task_id: str, fields: InputUpdateTask) -> None:
+        return self._session.make_request(
+            "PATCH", path=f"/api/v1/task/{task_id}", json=fields
+        )
+
+    def bulk_update(self, fields: InputBulkUpdateTask) -> None:
+        return self._session.make_request(
+            "PATCH", path="/api/v1/task/_bulk", json=fields
+        )
+
+    def get_required_actions(self, task_id: str) -> dict:
+        return self._session.make_request(
+            "GET", path=f"/api/v1/task/{task_id}/actionRequired"
+        )
+
+    def set_as_required(self, task_id: str, org_id: str) -> None:
+        return self._session.make_request(
+            "PUT", f"/api/v1/task/{task_id}/actionRequired/{org_id}"
+        )
+
+    def set_as_done(self, task_id: str, org_id: str) -> None:
+        return self._session.make_request(
+            "PUT", f"/api/v1/task/{task_id}/actionDone/{org_id}"
+        )
+
+    def share(self):
+        raise NotImplementedError()
+
+    def list_shares(self):
+        raise NotImplementedError()
+
+    def unshare(self):
+        raise NotImplementedError()
+
+    def find(
+        self,
+        filters: FilterExpr = None,
+        sortby: SortExpr = None,
+        paginate: Paginate = None,
+    ) -> List[OutputTask]:
+        query: QueryExpr = [
+            {"_name": "listTask"},
+            *self._build_subquery(filters=filters, sortby=sortby, paginate=paginate),
+        ]
+
+        return self._session.make_request(
+            "POST",
+            path="/api/v1/query",
+            params={"name": "tasks"},
+            json={"query": query},
+        )
+
+    def count(self, filters: FilterExpr = None) -> int:
+        query: QueryExpr = [
+            {"_name": "listTask"},
+            *self._build_subquery(filters=filters),
+            {"_name": "count"},
+        ]
+
+        return self._session.make_request(
+            "POST",
+            path="/api/v1/query",
+            params={"name": "task.count"},
+            json={"query": query},
+        )
+
+    def create_log(self, task_id: str, task_log: InputTaskLog) -> OutputTaskLog:
+        return self._session.make_request(
+            "POST", path=f"/api/v1/task/{task_id}/log", json=task_log
+        )
 
     def find_logs(
         self,
@@ -21,7 +103,7 @@ class TaskEndpoint(EndpointBase):
         filters: FilterExpr = None,
         sortby: SortExpr = None,
         paginate: Paginate = None,
-    ) -> List[dict]:
+    ) -> List[OutputTaskLog]:
         query: QueryExpr = [
             {"_name": "getTask", "idOrName": task_id},
             {"_name": "logs"},
---
+++ thehive4py/types/task_log.py
@@ -0,0 +1,28 @@
+from typing import List, TypedDict
+
+
+class InputTaskLogRequired(TypedDict):
+    message: str
+
+
+class InputTaskLog(InputTaskLogRequired, total=False):
+    startDate: int
+    includeInTimeline: int
+
+
+class OutputTaskLogRequired(TypedDict):
+    _id: str
+    _type: str
+    _createdBy: str
+    _createdAt: int
+    message: str
+    date: int
+    owner: str
+    extraData: dict
+
+
+class OutputTaskLog(OutputTaskLogRequired, total=False):
+    _updatedBy: str
+    _updatedAt: int
+    attachments: List[dict]  # TODO: typehint
+    includeInTimeline: int
