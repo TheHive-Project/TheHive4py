b9054f7e68035fc4423f6fb4f7f2b32466d04fce
Kamforka <kamforka@hotmail.com>
2022-02-22 20:26:25 +0100

    Test alert endpoints

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
+import time
+from typing import List
 
 import pytest
 from thehive4py.client import TheHiveApi
+from thehive4py.types.alert import InputAlert, OutputAlert
+from thehive4py.types.case import InputCase, OutputCase
 
 from tests.utils import Container, reinit_hive_container, spawn_hive_container
 
@@ -11,10 +15,77 @@ def thehive_container():
     yield container
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="function", autouse=True)
+def init_hive_container(thehive_container: Container):
+    client = TheHiveApi(
+        url=thehive_container.url, username="admin@org1.test", password="secret"
+    )
+    reinit_hive_container(client)
+
+
+@pytest.fixture(scope="session")
 def thehive(thehive_container: Container):
     client = TheHiveApi(
-        url=thehive_container.url, username="admin@test-org-1", password="admin"
+        url=thehive_container.url, username="admin@org1.test", password="secret"
+    )
+    return client
+
+
+@pytest.fixture
+def test_alert(thehive: TheHiveApi) -> OutputAlert:
+    return thehive.alert.create(
+        alert={
+            "title": "my first alert",
+            "description": "...",
+            "type": "test",
+            "source": "test",
+            "sourceRef": "first",
+            "externalLink": "http://",
+            "date": int(time.time() * 1000),
+            "tags": ["whatever"],
+        }
+    )
+
+
+@pytest.fixture
+def test_alerts(thehive: TheHiveApi) -> List[OutputAlert]:
+    alerts: List[InputAlert] = [
+        {
+            "title": "my first alert",
+            "description": "...",
+            "type": "test",
+            "source": "test",
+            "sourceRef": "first",
+            "date": int(time.time() * 1000),
+        },
+        {
+            "title": "my second alert",
+            "description": "...",
+            "type": "test",
+            "source": "test",
+            "sourceRef": "second",
+            "date": int(time.time() * 1000),
+        },
+    ]
+    return [thehive.alert.create(alert=alert) for alert in alerts]
+
+
+@pytest.fixture
+def test_case(thehive: TheHiveApi) -> OutputCase:
+    return thehive.case.create(
+        case={"title": "my first case", "description": "...", "tags": ["whatever"]}
+    )
+
+
+@pytest.fixture
+def test_cases(thehive: TheHiveApi) -> List[OutputCase]:
+    cases: List[InputCase] = [
+        {"title": "my first case", "description": "...", "tags": ["whatever"]},
+        {"title": "my second case", "description": "...", "tags": ["whatever"]},
+    ]
+    return [thehive.case.create(case=case) for case in cases]
+
+
     )
     reinit_hive_container(client)
     yield client
---
+++ tests/test_alert_endpoint.py
@@ -0,0 +1,139 @@
+from typing import List
+
+import pytest
+from thehive4py.client import TheHiveApi
+from thehive4py.errors import TheHiveError
+from thehive4py.query.filters import Eq
+from thehive4py.query.sort import Asc
+from thehive4py.types.alert import InputBulkUpdateAlert, InputUpdateAlert, OutputAlert
+from thehive4py.types.case import OutputCase
+
+
+class TestAlertEndpoint:
+    def test_create_and_get(self, thehive: TheHiveApi):
+        created_alert = thehive.alert.create(
+            {
+                "title": "my first alert",
+                "description": "...",
+                "type": "test",
+                "source": "test",
+                "sourceRef": "first",
+                "externalLink": "http://",
+                "date": 123,
+                "tags": ["whatever"],
+            }
+        )
+
+        fetched_alert = thehive.alert.get(created_alert["_id"])
+        fetched_alert.pop("_updatedAt")
+        fetched_alert.pop("_updatedBy")
+        assert created_alert == fetched_alert
+
+    def test_update(self, thehive: TheHiveApi, test_alert: OutputAlert):
+
+        alert_id = test_alert["_id"]
+        update_fields: InputUpdateAlert = {
+            "title": "my updated alert",
+            "description": "my updated description",
+        }
+        thehive.alert.update(alert_id=alert_id, fields=update_fields)
+        updated_alert = thehive.alert.get(alert_id=alert_id)
+
+        assert updated_alert["title"] == update_fields["title"]
+        assert updated_alert["description"] == update_fields["description"]
+
+    def test_bulk_update(self, thehive: TheHiveApi, test_alerts: List[OutputAlert]):
+
+        alert_ids = [alert["_id"] for alert in test_alerts]
+        update_fields: InputBulkUpdateAlert = {
+            "ids": alert_ids,
+            "title": "my updated alert #1",
+            "description": "my updated description #1",
+        }
+
+        thehive.alert.bulk_update(fields=update_fields)
+        updated_alerts = thehive.alert.find()
+
+        for updated_alert in updated_alerts:
+            assert updated_alert["title"] == update_fields["title"]
+            assert updated_alert["description"] == update_fields["description"]
+
+    def test_follow_and_unfollow(self, thehive: TheHiveApi, test_alert: OutputAlert):
+        alert_id = test_alert["_id"]
+
+        thehive.alert.unfollow(alert_id=alert_id)
+        unfollowed_alert = thehive.alert.get(alert_id=alert_id)
+        assert unfollowed_alert["follow"] is False
+
+        thehive.alert.follow(alert_id=alert_id)
+        followed_alert = thehive.alert.get(alert_id=alert_id)
+        assert followed_alert["follow"] is True
+
+    def test_promote_to_case(self, thehive: TheHiveApi, test_alert: OutputAlert):
+        alert_id = test_alert["_id"]
+
+        case_from_alert = thehive.alert.promote_to_case(alert_id=alert_id)
+        promoted_alert = thehive.alert.get(alert_id=alert_id)
+        assert promoted_alert.get("caseId") == case_from_alert["_id"]
+
+    def test_merge_into_case(
+        self, thehive: TheHiveApi, test_alert: OutputAlert, test_case: OutputCase
+    ):
+        alert_id = test_alert["_id"]
+        case_id = test_case["_id"]
+
+        merged_case = thehive.alert.merge_into_case(alert_id=alert_id, case_id=case_id)
+
+        merged_alert = thehive.alert.get(alert_id=alert_id)
+        assert merged_alert.get("caseId") == merged_case["_id"]
+
+    def test_bulk_merge_into_case(
+        self, thehive: TheHiveApi, test_alerts: List[OutputAlert], test_case: OutputCase
+    ):
+        alert_ids = [alert["_id"] for alert in test_alerts]
+        case_id = test_case["_id"]
+
+        merged_case = thehive.alert.bulk_merge_into_case(
+            case_id=case_id, alert_ids=alert_ids
+        )
+
+        for alert in thehive.alert.find():
+            assert alert.get("caseId") == merged_case["_id"]
+
+    def test_find_and_count(self, thehive: TheHiveApi, test_alerts: List[OutputAlert]):
+        found_alerts = thehive.alert.find(
+            filters=Eq("title", test_alerts[0]["title"])
+            | Eq("title", test_alerts[1]["title"]),
+            sortby=Asc("_createdAt"),
+        )
+        alert_count = thehive.alert.count()
+
+        for alert in found_alerts:
+            alert.pop("_updatedAt")
+            alert.pop("_updatedBy")
+
+        assert found_alerts == test_alerts
+        assert len(found_alerts) == alert_count
+
+    def test_delete(self, thehive: TheHiveApi, test_alert: OutputAlert):
+        alert_id = test_alert["_id"]
+        thehive.alert.delete(alert_id)
+        with pytest.raises(TheHiveError):
+            thehive.alert.get(alert_id)
+
+    def test_bulk_delete(self, thehive: TheHiveApi, test_alerts: List[OutputAlert]):
+        alert_ids = [alert["_id"] for alert in test_alerts]
+        thehive.alert.bulk_delete(ids=alert_ids)
+        for alert_id in alert_ids:
+            with pytest.raises(TheHiveError):
+                thehive.alert.get(alert_id)
+
+    def test_create_and_get_observable(
+        self, thehive: TheHiveApi, test_alert: OutputAlert
+    ):
+
+        created_observables = thehive.alert.create_observable(
+            test_alert["_id"], {"dataType": "domain", "data": "example.com"}
+        )
+        alert_observables = thehive.alert.find_observables(test_alert["_id"])
+        assert created_observables == alert_observables
