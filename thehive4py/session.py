20e8772f84f4aa7057eb3392660193f5d5c6df9a
Kyle Jackson <kyle.jackson@octopus.com>
2022-10-10 16:34:16 +1000

    Updated session to use simplejson

--- thehive4py/session.py
+++ thehive4py/session.py
@@ -1,6 +1,6 @@
 import json as jsonlib
 from collections import UserDict
-from requests import JSONDecodeError
+from simplejson import JSONDecodeError
 from os import PathLike
 from typing import Any, Optional, Union
 
