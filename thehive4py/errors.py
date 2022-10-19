6547f028449ffebf5d35a01eeb875208d07e5807
kamforka <kamforka@hotmail.com>
2022-10-19 14:12:59 +0200

    Persist error response data in TheHiveError

--- thehive4py/errors.py
+++ thehive4py/errors.py
@@ -1,2 +1,14 @@
+from typing import Optional
+
+from requests import Response
+
+
 class TheHiveError(Exception):
     """Base error class for TheHive API."""
+
+    def __init__(
+        self, message: str, response: Optional[Response] = None, *args, **kwargs
+    ):
+        super().__init__(message, *args, **kwargs)
+        self.message = message
+        self.response = response
