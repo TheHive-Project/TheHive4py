7e5e54b1b90145dacd8a21d962927d88186f3214
Mickael BRUNEL <mickael_brunel@hotmail.fr>
2022-12-21 09:25:04 +0100

    Rename attachment_path to observable_path
    Renaming of the variable attachment_path to observable_path to be better align with the existing name conventions.

--- thehive4py/endpoints/observable.py
+++ thehive4py/endpoints/observable.py
@@ -113,11 +113,11 @@ class ObservableEndpoint(EndpointBase):
         )
 
     def download_attachment(
-        self, observable_id: str, attachment_id: str, attachment_path: str, as_zip: bool = False
+        self, observable_id: str, attachment_id: str, observable_path: str, as_zip: bool = False
     ) -> None:
         return self._session.make_request(
             "GET",
             path=f"/api/v1/observable/{observable_id}/attachment/{attachment_id}/download",
             params={"asZip": as_zip},
-            download_path=attachment_path,
+            download_path=observable_path,
         )
