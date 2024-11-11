2df4bee52518742b601f049082030009288b8758
Kamforka <antalszabolcs01@gmail.com>
2024-11-11 19:32:14 +0100

    #364 - Fix alert docs

--- examples/alert/advanced.py
+++ examples/alert/advanced.py
@@ -1,13 +1,17 @@
 from thehive4py import TheHiveApi
+from thehive4py.types.alert import InputAlert
 
 hive = TheHiveApi(url="http://localhost:9000", apikey="h1v3b33")
 
-simple_alert = hive.alert.create(
-    alert={
-        "type": "simple",
-        "source": "tutorial",
-        "sourceRef": "should-be-unique",
-        "title": "a simple alert",
-        "description": "a bit too simple",
-    }
-)
+input_alert: InputAlert = {
+    "type": "advanced",
+    "source": "tutorial",
+    "sourceRef": "should-be-unique",
+    "title": "an advanced alert",
+    "description": "a bit more advanced",
+    "tags": ["advanced", "example"],
+    "severity": 1,
+    "caseTemplate": "my-template",
+}
+
+output_alert = hive.alert.create(alert=input_alert)
