53ff122a005db1d9b8e99a0df52252c3e7e1c294
Juan <43722131+JuanTecedor@users.noreply.github.com>
2025-01-06 23:15:49 +0100

    Add Has to query __init__

--- thehive4py/query/__init__.py
+++ thehive4py/query/__init__.py
@@ -2,7 +2,7 @@ from typing import List, Union
 
 from .filters import Between, Contains, EndsWith, Eq
 from .filters import FilterExpr as _FilterExpr
-from .filters import Gt, Gte, Id, In, Like, Lt, Lte, Match, Ne, StartsWith
+from .filters import Gt, Gte, Has, Id, In, Like, Lt, Lte, Match, Ne, StartsWith
 from .page import Paginate
 from .sort import Asc, Desc
 from .sort import SortExpr as _SortExpr
