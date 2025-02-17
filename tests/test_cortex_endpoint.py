58580ab015348aed924014900735c0aac52ffd78
Juan <43722131+JuanTecedor@users.noreply.github.com>
2025-02-17 18:09:49 +0100

    Fix InputAnalyzerJob type and add regression test

--- tests/test_cortex_endpoint.py
+++ tests/test_cortex_endpoint.py
@@ -1,8 +1,21 @@
 from thehive4py.client import TheHiveApi
 from thehive4py.types.case import OutputCase
+from thehive4py.types.observable import OutputObservable
 
 
 class TestCortexEndpoint:
+    def test_create_analyzer_job(
+        self, thehive: TheHiveApi, test_observable: OutputObservable
+    ):
+        output_analyzer_job = thehive.cortex.create_analyzer_job(
+            job={
+                "analyzerId": "example",
+                "cortexId": "cortex",
+                "artifactId": test_observable["_id"],
+            }
+        )
+        assert output_analyzer_job["_type"] == "case_artifact_job"
+
     def test_list_analyzers(self, thehive: TheHiveApi):
         analyzers = thehive.cortex.list_analyzers()
         assert analyzers == []
