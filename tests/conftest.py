ae3f1fec62a13c4bd1222616a9804c15568349e5
Szabolcs Antal <antalszabolcs01@gmail.com>
2025-11-26 17:31:07 +0100

    #523 - Use TheHive v5.5.13 in integration tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,7 +25,7 @@ from thehive4py.types.user import OutputUser
 @pytest.fixture(scope="session")
 def test_config():
     return TestConfig(
-        image_name="strangebee/thehive:5.5.10",
+        image_name="strangebee/thehive:5.5.13",
         container_name="thehive4py-integration-tester",
         user="admin@thehive.local",
         password="secret",
